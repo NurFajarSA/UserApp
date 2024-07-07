3d86018cccb072b2260686e3e245219ea95ca450
Nur Fajar Sayyidul Ayyam <nur.fajar11@ui.ac.id>
2024-07-08 00:57:23 +0700

    fix: Drawer toggle bug

--- src/app/components/common/drawer/index.tsx
+++ src/app/components/common/drawer/index.tsx
@@ -1,20 +1,40 @@
-import React, { PropsWithChildren } from 'react';
+import React, { PropsWithChildren, useEffect, useState } from 'react';
 
 interface DrawerProps {
     sidebarContent: React.ReactNode;
-    isOpen?: boolean;
+    isOpen: boolean;
 }
 
 const Drawer: React.FC<PropsWithChildren<DrawerProps>> = ({ sidebarContent, children, isOpen }) => {
+    const [isLargeScreen, setIsLargeScreen] = useState<boolean>(false);
     const drawerClass = isOpen ? 'drawer lg:drawer-open' : '';
+    console.log('Drawer is open:', isOpen);
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (typeof window !== 'undefined') {
+                setIsLargeScreen(window.innerWidth >= 1024);
+            }
+        };
+
+        handleResize();
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
     return (
         <div className={drawerClass}>
-            <input id="my-drawer-2" type="checkbox" className="drawer-toggle hidden md:block" />
+            {isLargeScreen && <div className='drawer-toggle'/>}
+            {!isLargeScreen && (
+                <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+            )}
             <div className="drawer-content flex flex-col">
                 {children}
             </div>
             <div className="drawer-side">
-                <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
+                {!isLargeScreen && (
+                    <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
+                )}
                 <ul className="menu bg-base-100 text-base-content min-h-full w-60 p-4">
                     <a className="btn btn-ghost text-xl">UserApp</a>
                     <br />
