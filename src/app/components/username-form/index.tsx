524d7258b7d801c33f59bd2916407b1e31bcd901
Nur Fajar Sayyidul Ayyam <nur.fajar11@ui.ac.id>
2024-07-08 13:47:20 +0700

    feat: Add username form

---
+++ src/app/components/username-form/index.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+interface UsernameFormProps {
+    username: string;
+}
+
+const UsernameForm: React.FC<UsernameFormProps> = ({ username }) => {
+    const [newUsername, setNewUsername] = useState(username);
+
+    return (
+        <div className="flex flex-col gap-4 my-card">
+            <div className="text-md font-medium">Ubah Username</div>
+            <form className="flex justify-between">
+                <label className="input input-bordered flex items-center gap-2 w-3/4 mr-6">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 16 16"
+                        fill="currentColor"
+                        className="h-4 w-4 opacity-70">
+                        <path
+                            d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                    </svg>
+                    <input
+                        type="text"
+                        placeholder="Username"
+                        value={newUsername}
+                        onChange={(e) => {
+                            setNewUsername(e.target.value);
+                        }}
+                    />
+                </label>
+                {newUsername !== username && (<button className="btn btn-primary w-1/4" type="submit">Ubah</button>)}
+                {newUsername === username && (<button className="btn btn-primary w-1/4" type="submit" disabled>Ubah</button>)}
+            </form>
+        </div>
+    );
+}
+
+export default UsernameForm;
