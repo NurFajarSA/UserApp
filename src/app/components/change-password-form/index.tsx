0817a0471febe9fff952fdfe74fdad85cf2847b6
Nur Fajar Sayyidul Ayyam <nur.fajar11@ui.ac.id>
2024-07-09 15:51:18 +0700

    feat: Add old pass field

--- src/app/components/change-password-form/index.tsx
+++ src/app/components/change-password-form/index.tsx
@@ -1,14 +1,41 @@
 import { useEffect, useState } from "react";
 
 const ChangePasswordForm: React.FC = () => {
+    const [oldPassword, setOldPassword] = useState("");
     const [newPassword, setNewPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
+
+    const handleChangePassword = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log("Old Password:", oldPassword);
+        console.log("New Password:", newPassword);
+    }
 
     return (
         <div className="flex flex-col gap-4 my-card">
             <div className="text-md font-medium">Ubah Password</div>
-            <form className="flex justify-between items-end">
+            <form className="flex justify-between items-end" onSubmit={handleChangePassword}>
                 <div className="flex flex-col gap-4 w-3/4 mr-6 ">
+                    <label className="input input-bordered flex items-center gap-2">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 16 16"
+                            fill="currentColor"
+                            className="h-4 w-4 opacity-70">
+                            <path
+                                fillRule="evenodd"
+                                d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                                clipRule="evenodd" />
+                        </svg>
+                        <input
+                            type="password"
+                            placeholder="Password Lama"
+                            value={oldPassword}
+                            onChange={(e) => {
+                                setOldPassword(e.target.value);
+                            }}
+                        />
+                    </label>
                     <label className="input input-bordered flex items-center gap-2">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
